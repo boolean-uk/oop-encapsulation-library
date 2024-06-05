1493b57c43222de80b03fc83a76a57b5c8402ba1
Will Baxter <bax.will@googlemail.com>
2024-06-05 12:25:36 +0100

    Removed unit test for books being added without author details, added test for author details and functionality

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -1,33 +1,60 @@
-import { Library, Book, Author } from '../src/library.js'
+import { Library, Book, Author, Publisher } from "../src/library.js";
 
-describe ('Library', ()=> {
-    let library;
+describe("Library", () => {
+  let library;
 
-    beforeEach(()=> {
-        library = new Library()
-    })
+  beforeEach(() => {
+    library = new Library();
+  });
 
-    it('should be a class that can be instantiated', () => {
-        expect(library).toBeInstanceOf(Library)
-    })
+  it("should be a class that can be instantiated", () => {
+    expect(library).toBeInstanceOf(Library);
+  });
 
-    it('should have an add book function that requires a book with title, author, and publication date', () => {
-        library.addBook(new Book('The Grapes of Wrath', 'John Steinbeck', '1939', 'classic fiction'))
+  it("should throw an error if we add a book without a name, author, genre, or year", () => {
+    expect(() => {
+      library.addBook(
+        new Book("", "John Steinbeck", "1939", "classic fiction")
+      );
+    }).toThrowError(
+      "This library requires a book with a title, author, genre, and year"
+    );
+  });
 
-        expect(library.books[0].title).toEqual('The Grapes of Wrath')
-    })
+  it("should accept a book with an author class as its author", () => {
+    const myAuthor = new Author("Robin Hobb", "60", "robin@hobb.com");
+    const myBook = new Book(
+      "Assassin's Apprentice",
+      myAuthor,
+      "2001",
+      "fantasy"
+    );
+    library.addBook(myBook);
 
-    it('should throw an error if we add a book without a name, author, genre, or year', () => {
+    expect(library.books[0].author.name).toEqual("Robin Hobb");
+  });
 
-        expect(() => {library.addBook(new Book('', 'John Steinbeck', '1939', 'classic fiction'))}).toThrowError('This library requires a book with a title, author, genre, and year')
-    })
-})
+  it("should reject a book if its author it misses either a name, age, or email", () => {
+    const myAuthor = new Author("Robin Hobb", "", "robin@hobb.com");
+    const myBook = new Book(
+      "Assassin's Apprentice",
+      myAuthor,
+      "2001",
+      "fantasy"
+    );
+    
+    expect(() => {
+      library.addBook(myBook);
+    }).toThrowError(
+      "Books being added to this library require the author to have a name, age, and email address"
+    );
+  });
+});
 
-describe ('Author', ()=> {
-    it('should have a name, age, and email', () => {
-        const myAuthor = new Author('Robin Hobb', '60', 'robin@hobb.com')
+describe("Author", () => {
+  it("should have a name, age, and email", () => {
+    const myAuthor = new Author("Robin Hobb", "60", "robin@hobb.com");
 
-        expect (myAuthor.name).toEqual('Robin Hobb')
-    })
-
-})
+    expect(myAuthor.name).toEqual("Robin Hobb");
+  });
+});
--- src/library.js
+++ src/library.js
@@ -15,6 +15,11 @@ class Library {
         'This library requires a book with a title, author, genre, and year'
       );
     }
+
+    if (!book.author.name || !book.author.age || !book.author.email) {
+        throw new Error('Books being added to this library require the author to have a name, age, and email address')
+    }
+    
     this.#books.push(book);
   }
 }
@@ -37,5 +42,13 @@ class Author {
     }
 }
 
+class Publisher {
+    constructor(name, website, collection) {
+        this.name = name
+        this.website = website
+        this.collection = collection
+    }
+}
 
-export { Library, Book, Author };
+
+export { Library, Book, Author, Publisher };
