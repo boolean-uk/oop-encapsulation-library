7ed42af29b9d427d3e29c9dc745a51841bc11310
Leonardo Lodi <your_email@address>
2024-06-05 17:33:49 +0100

    add author class properties and test it

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -1,4 +1,4 @@
-import Library, { Book } from "../src/library.js";
+import Library from "../src/library.js";
 
 describe('Library', () => {
     let library
@@ -8,77 +8,97 @@ describe('Library', () => {
     })
 
     it('should be able to add a book to the collection', () => {
-        library.addBook('A Game of Thrones', 'George R. R. Martin', 'Fantasy', 'August 1, 1996')
+        library.addBook('A Game of Thrones', 'George R. R. Martin', 75, 'georgemartin@gmail.com', 'Fantasy', 'August 1, 1996')
 
-        expect(library.collectionList()).toEqual([
+        expect(library.listCollection()).toEqual([
             {
                 title: 'A Game of Thrones',
-                author: 'George R. R. Martin',
+                author: {
+                    name: 'George R. R. Martin',
+                    age: 75,
+                    email: 'georgemartin@gmail.com'
+                },
                 genre: 'Fantasy',
-                publicationDate: 'August 1, 1996'
+                release: 'August 1, 1996'
             }
         ])
     })
 
     it('should be able to remove an book from the collection', () => {
-        library.addBook('A Game of Thrones', 'George R. R. Martin', 'Fantasy', 'August 1, 1996')
-        library.addBook("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 'Fantasy', 'June 26, 1997')
+        library.addBook('A Game of Thrones', 'George R. R. Martin', 75, 'georgemartin@gmail.com', 'Fantasy', 'August 1, 1996')
+        library.addBook("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 58, 'jkrowling@gmail.com', 'Fantasy', 'June 26, 1997')
 
         library.removeBook('A Game of Thrones')
 
-        expect(library.collectionList()).toEqual([
+        expect(library.listCollection()).toEqual([
             {
                 title: "Harry Potter and the Philosopher's Stone",
-                author: 'J. K. Rowling',
+                author: {
+                    name: 'J. K. Rowling',
+                    age: 58,
+                    email: 'jkrowling@gmail.com'
+                },
                 genre: 'Fantasy',
-                publicationDate: 'June 26, 1997'
+                release: 'June 26, 1997'
             }
         ])
     })
 
     it('should be able to list the collection', () => {
-        library.addBook('A Game of Thrones', 'George R. R. Martin', 'Fantasy', 'August 1, 1996')
-        library.addBook("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 'Fantasy', 'June 26, 1997')
+        library.addBook('A Game of Thrones', 'George R. R. Martin', 75, 'georgemartin@gmail.com', 'Fantasy', 'August 1, 1996')
+        library.addBook("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 58, 'jkrowling@gmail.com', 'Fantasy', 'June 26, 1997')
 
-        expect(library.collectionList()).toEqual([
+        expect(library.listCollection()).toEqual([
             {
                 title: 'A Game of Thrones',
-                author: 'George R. R. Martin',
+                author: {
+                    name: 'George R. R. Martin',
+                    age: 75,
+                    email: 'georgemartin@gmail.com'
+                },
                 genre: 'Fantasy',
-                publicationDate: 'August 1, 1996'
+                release: 'August 1, 1996'
             },
             {
                 title: "Harry Potter and the Philosopher's Stone",
-                author: 'J. K. Rowling',
+                author: {
+                    name: 'J. K. Rowling',
+                    age: 58,
+                    email: 'jkrowling@gmail.com'
+                },
                 genre: 'Fantasy',
-                publicationDate: 'June 26, 1997'
+                release: 'June 26, 1997'
             }
         ])
     })
 
     it("should be only able to modify book's genre", () => {
-        library.addBook('A Game of Thrones', 'George R. R. Martin', 'Drama', 'August 1, 1996')
+        library.addBook('A Game of Thrones', 'George R. R. Martin', 75, 'georgemartin@gmail.com', 'Drama', 'August 1, 1996')
 
         expect(() => library.collection[0].title = 'The Hobbit').toThrowError()
-        expect(() => library.collection[0].author = 'J. R. R. Tolkien').toThrowError()
-        expect(() => library.collection[0].publicationDate = 'September 21, 1937').toThrowError()
+        expect(() => library.collection[0].author.name = 'J. R. R. Tolkien').toThrowError()
+        expect(() => library.collection[0].author.age = 62).toThrowError()
+        expect(() => library.collection[0].author.email = 'jkrowling@gmail.com').toThrowError()
+        expect(() => library.collection[0].release = 'September 21, 1937').toThrowError()
 
         library.collection[0].genre = 'Fantasy'
 
-        expect(library.collectionList()).toEqual([
+        expect(library.listCollection()).toEqual([
             {
                 title: 'A Game of Thrones',
-                author: 'George R. R. Martin',
+                author: {
+                    name: 'George R. R. Martin',
+                    age: 75,
+                    email: 'georgemartin@gmail.com'
+                },
                 genre: 'Fantasy',
-                publicationDate: 'August 1, 1996'
+                release: 'August 1, 1996'
             }
         ])
     })
 
-    it("should throw an error if the book's title, author, genre or publicationDate not provided correctly", () => {
-        expect(() => library.addBook('A Game of Thrones', '', 'Drama', 'August 1, 1996')).toThrowError("Book's properties should be provided correctly")
-
-        expect(() => library.addBook('A Game of Thrones', 'George R. R. Martin', 'Drama', 1996)).toThrowError("Book's properties should be provided correctly")
+    it("should throw an error if the book's title, author, genre or release not provided correctly", () => {
+        expect(() => library.addBook('A Game of Thrones', 'George R. R. Martin', 75, 'georgemartin@gmail.com', 'Fantasy', 1996)).toThrowError("Book's properties should be provided correctly")
     }) 
 
     it('should throw an error if trying to remove an unfound book', () => {
--- src/library.js
+++ src/library.js
@@ -9,17 +9,30 @@ class Library {
         return this.#collection
     }
 
-    addBook(title, author, genre, publicationDate) {
-        if ([title, author, genre, publicationDate].some(prop => typeof prop !== 'string' || prop === '')) {
-            throw new Error("Book's properties should be provided correctly")
+    addBook(title, authorName, authorAge, authorEmail, genre, release) {
+        if (
+          [title, authorName, authorEmail, genre, release].some(
+            (prop) => typeof prop !== 'string' || prop === ''
+          ) ||
+          typeof authorAge !== 'number'
+        ) {
+          throw new Error("Book's properties should be provided correctly")
         }
 
-        const bookToAdd = new Book(title, author, genre, publicationDate)
+        const bookToAdd = new Book(
+          title,
+          new Author(authorName, authorAge, authorEmail),
+          genre,
+          release
+        )
+        
         this.#collection.push(bookToAdd)
     }
 
     removeBook(title) {
-        const bookToRemove = this.#collection.findIndex(book => book.title === title)
+        const bookToRemove = this.#collection.findIndex(
+          (book) => book.title === title
+        )
 
         if (bookToRemove === -1) {
             throw new Error('Book not found')
@@ -28,13 +41,17 @@ class Library {
         this.#collection.splice(bookToRemove, 1)
     }
 
-    collectionList() {
+    listCollection() {
         return this.#collection.map(book => (
             {
                 title: book.title,
-                author: book.author,
+                author: {
+                    name: book.author.name,
+                    age: book.author.age,
+                    email: book.author.email
+                },
                 genre: book.genre,
-                publicationDate: book.publicationDate
+                release: book.release
             }
         ))
     }
@@ -44,13 +61,13 @@ class Book {
     #title
     #author
     #genre
-    #publicationDate
+    #release
 
-    constructor(title, author, genre, publicationDate) {
+    constructor(title, author, genre, release) {
         this.#title = title
         this.#author = author
         this.#genre = genre
-        this.#publicationDate = publicationDate
+        this.#release = release
     }
 
     get title() {
@@ -69,10 +86,33 @@ class Book {
         this.#genre = newGenre
     }
 
-    get publicationDate() {
-        return this.#publicationDate
+    get release() {
+        return this.#release
     }
 }
 
-export { Book }
+class Author {
+    #name
+    #age
+    #email
+
+    constructor(name, age, email) {
+        this.#name = name
+        this.#age = age
+        this.#email = email
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    get age() {
+        return this.#age
+    }
+
+    get email() {
+        return this.#email
+    }
+}
+
 export default Library
