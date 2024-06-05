d4fbdcb67f911302a23b7ca3a320793821d15a3f
Tim Zoltie <t.zoltie@gmail.com>
2024-06-05 13:06:58 +0100

    remove method

--- src/library.js
+++ src/library.js
@@ -3,30 +3,34 @@ class Book {
     this.title = title;
     this.author = author;
     this.publicationDate = date;
-    Object.freeze(this)
+    Object.freeze(this);
   }
 }
 
-
-
 class Library {
-    #books;
-    constructor() {
-      this.#books = [];
-    }
-    set books(book) {
-      if(typeof book !== 'object') {
-          throw 'book is not valid'
-      }
-      this.#books.push(book)
-    }
-    add(book) {
-      this.#books.push(book);
-    }
-    get books() {
-        return [...this.#books];
-      }
-   
+  #books;
+  constructor() {
+    this.#books = [];
   }
-  
- 
+  set books(book) {
+    if (typeof book !== "object") {
+      throw "book is not valid";
+    }
+    this.#books.push(book);
+  }
+  add(book) {
+    this.#books.push(book);
+  }
+  get books() {
+    return [...this.#books];
+  }
+  remove(book) {
+    const found = this.#books.find((b) => b.title === book.title);
+    const index = this.#books.indexOf(book);
+    const newLibray = this.#books.splice(index, 1);
+    if (!found) {
+      throw "book not found";
+    }
+    return newLibray;
+  }
+}
