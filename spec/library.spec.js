f0cc7d3a9b84170d0a10e1f416167e3642e032d7
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-05 17:17:59 +0200

    create tests for error throwing

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -25,7 +25,7 @@ describe('Library', () => {
 
     it('should be able to remove a book', () => {
         const john = new Author('John Flanagan', 40, 'johnny@hotmail.com')
-        
+
         library.addBook('Game of Thrones', george, 'Fantasy', '01-08-1996', penguin)
         library.addBook('The Ruins of Gorlan', john, 'Fantasy', '01-11-2004',penguin)
 
@@ -34,5 +34,18 @@ describe('Library', () => {
         expect(library.books.length).toBe(1)
         expect(library.books[0].title).toBe('Game of Thrones')
         expect(removed.title).toBe('The Ruins of Gorlan')
+    })
+
+    it('should throw an error when not providing all author and publisher details when adding a book', () => {
+        const john = new Author('John Flanagan', 40,)
+        const banana = new Publisher('banana',)
+
+        expect(() => library.addBook('The Ruins of Gorlan', john, 'Fantasy', '01-11-2004',penguin)).toThrow(
+            'author must have a name, age and email'
+          )
+
+          expect(() => library.addBook('Game of Thrones', george, 'Fantasy', '01-08-1996', banana)).toThrow(
+            'publisher must have a name and website'
+          )
     })
 })
