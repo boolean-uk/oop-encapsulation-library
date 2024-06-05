09844f06fb6a83ce55eda38b96de7a2f5c5d9389
Will Baxter <bax.will@googlemail.com>
2024-06-05 12:02:03 +0100

    Add book functionality that throws error if no author, title, genre, or year

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -1,4 +1,4 @@
-import Library from '../src/library.js'
+import { Library, Book } from '../src/library.js'
 
 describe ('Library', ()=> {
     let library;
@@ -9,5 +9,16 @@ describe ('Library', ()=> {
 
     it('should be a class that can be instantiated', () => {
         expect(library).toBeInstanceOf(Library)
+    })
+
+    it('should have an add book function that requires a book with title, author, and publication date', () => {
+        library.addBook(new Book('The Grapes of Wrath', 'John Steinbeck', '1939', 'classic fiction'))
+
+        expect(library.books[0].title).toEqual('The Grapes of Wrath')
+    })
+
+    it('should throw an error if we add a book without a name, author, genre, or year', () => {
+
+        expect(() => {library.addBook(new Book('', 'John Steinbeck', '1939', 'classic fiction'))}).toThrowError('This library requires a book with a title, author, genre, and year')
     })
 })
--- src/library.js
+++ src/library.js
@@ -1,5 +1,37 @@
 class Library {
+  #books;
 
+  constructor(books = []) {
+    this.#books = books;
+  }
+
+  get books() {
+    return structuredClone(this.#books);
+  }
+
+  addBook(book) {
+    if (!book.title || !book.author || !book.publicationDate || !book.genre) {
+      throw new Error(
+        'This library requires a book with a title, author, genre, and year'
+      );
+    }
+    this.#books.push(book);
+  }
 }
 
-export default Library
+class Book {
+  constructor(title, author, publicationDate, genre, publisher) {
+    this.title = title;
+    this.author = author;
+    this.publicationDate = publicationDate;
+    this.genre = genre;
+    this.publisher = publisher;
+  }
+}
+
+// const library = new Library();
+// library.addBook(new Book("cheese", "tomato", "pies"));
+
+// console.log(library.books);
+
+export { Library, Book };
