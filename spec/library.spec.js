7c0170751f4201bc5fd26e6adf050b5bf3d10b9d
Leonardo Lodi <your_email@address>
2024-06-05 12:22:36 +0100

    add listBooks function and test it

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -35,4 +35,31 @@ describe('Library', () => {
             )
         ])
     })
+
+    it('should be able to list the books', () => {
+        library.addBook('A Game of Thrones', 'George R. R. Martin', 'Fantasy', 'August 1, 1996')
+        library.addBook("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 'Fantasy', 'June 26, 1997')
+        library.addBook('The Hobbit', 'J. R. R. Tolkien', 'Fantasy', 'September 21, 1937')
+
+        expect(library.listBooks()).toEqual([
+            new Book(
+                'A Game of Thrones', 
+                'George R. R. Martin', 
+                'Fantasy', 
+                'August 1, 1996'
+            ),
+            new Book(
+                "Harry Potter and the Philosopher's Stone", 
+                'J. K. Rowling', 
+                'Fantasy', 
+                'June 26, 1997'
+            ),
+            new Book(
+                'The Hobbit', 
+                'J. R. R. Tolkien', 
+                'Fantasy', 
+                'September 21, 1937'
+            )
+        ])
+    })
 })
