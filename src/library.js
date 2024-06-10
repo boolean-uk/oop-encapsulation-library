4758f1bca3399cfe55faa0dac3b2ba3cbc43baec
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-10 08:58:45 +0100

    core criteria complete

--- src/library.js
+++ src/library.js
@@ -1,74 +1,93 @@
-export default class Library{
-    #books
+export default class Library {
+    #books;
     #bookId
- constructor() {
-this.#books = []
-this.#bookId = 1
- }
-
- addBook(book) {
-    book.setId(this.#bookId)
-    this.#bookId++
-    this.#books.push(book);
-    return book.getId()
+    constructor() {
+      this.#books = [];
+      this.#bookId = 1
+    }
+  
+    addBook(book) {
+      book.id = this.#bookId
+      this.#bookId++
+      this.#books.push(book);
+      return book.id
+    }
+  
+    removeBook(bookId) {
+      let updatedBooks = this.#books.filter(book => book.id !== bookId)
+      this.#books = updatedBooks
+    }
+  
+    list() {
+      return this.#books
+    }
   }
-
-  list() {
-    return this.#books
-  }
-}
-
-class Book{
+  
+  export class Book {
     #title;
     #author;
+    #genre;
     #publicationDate;
     #publisher;
     #id;
     constructor(title, author, genre, publicationDate, publisher) {
-        this.#title = title;
-        this.#author = author;
-        this.genre = genre;
-        this.#publicationDate = publicationDate;
-        this.#publisher = publisher;
-      }
-    
-      getTitle() {
-        return this.#title;
-      }
-    
-      getAuthor() {
-        return this.#author;
-      }
-    
-      setGenre(genre) {
-        this.genre = genre;
-      }
-    
-      getPublicationDate(){
-        return this.#publicationDate
-      }
-    
-      getGenre() {
-        return this.genre;
-      }
-    
-      setId(id){
-        this.#id = id
-      }
-    
-      getId(){
-        this.#id
-      }
-
-
-}
-
-//class Author{}
-
-/*/class Publisher{
-    constructor(name, websiteURL){
-        this.name = name
-        this.websiteURL = websiteURL
+      this.#title = title;
+      this.#author = author;
+      this.#genre = genre;
+      this.#publicationDate = publicationDate;
+      this.#publisher = publisher;
+      this.#id = null
     }
-//}*/
-export { Book }
+  
+    get title() {
+      return this.#title;
+    }
+  
+    get author() {
+      return this.#author;
+    }
+  
+    set author(author){
+      return this.#author = author
+    }
+  
+    set genre(genre) {
+      this.#genre = genre;
+    }
+  
+    get publicationDate(){
+      return this.#publicationDate
+    }
+  
+    get genre() {
+      return this.#genre;
+    }
+  
+    set id(id){
+      this.#id = id
+    }
+  
+    get id(){
+      return this.#id
+    }
+  
+    get publisher(){
+      return this.#publisher
+    }
+  }
+  
+  export class Author {
+    constructor(name, age, email) {
+      this.name = name;
+      this.age = age;
+      this.email = email;
+    }
+  }
+  
+  export class Publisher {
+    constructor(name, websiteURL) {
+      this.name = name;
+      this.websiteURL = websiteURL;
+    }
+  }
+  
